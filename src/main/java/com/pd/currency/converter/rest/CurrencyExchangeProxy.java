ff477bf546d1ff33edd00149c9103be787eac615
Pratyush Dey <pratyushud@gmail.com>
2025-06-22 19:37:36 +0530

    kubernetes deployment file created

--- src/main/java/com/pd/currency/converter/rest/CurrencyExchangeProxy.java
+++ src/main/java/com/pd/currency/converter/rest/CurrencyExchangeProxy.java
@@ -5,7 +5,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name= "exchange-service")
+@FeignClient(name = "exchange-service", url = "${EXCHANGE_SERVICE_URI:localhost}:8000/")
 public interface CurrencyExchangeProxy {
 
     @GetMapping("/currency-exchange/from/{from}/to/{to}")
